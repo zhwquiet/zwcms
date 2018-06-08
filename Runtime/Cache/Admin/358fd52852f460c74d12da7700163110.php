950b9bdd30769b63773f0089b074b4e90c3b46b0
zhwquiet <admin@admindeMacBook-Pro.local>
2018-06-08 08:00:50 +0800

    删除日志和缓存文件

--- Runtime/Cache/Admin/358fd52852f460c74d12da7700163110.php
+++
@@ -1,7 +0,0 @@
-<?php if (!defined('THINK_PATH')) exit();?><ul class="templist">
-    <li class="tips"><span>提示：</span>模板选择请谨慎操作</li>
-    <?php if(is_array($list)): foreach($list as $key=>$item): ?><li class="list_item" attr_item="<?php echo ($item["value"]); ?>">
-            <span></span>
-            <a href="javaScript:void(0)"><?php echo ($item["value"]); ?></a>
-        </li><?php endforeach; endif; ?>
-</ul>
